0b0b8562c6d1d676c232e87ed8c63c48fed255b9
Matteo Ladislai <maladislai@paa.it>
2024-12-26 21:01:26 +0100

    Exploring static things

--- src/test/java/exerc/ExercMain.java
+++ src/test/java/exerc/ExercMain.java
@@ -1,19 +1,35 @@
 package exerc;
 
-public class ExercMain{
+import java.util.Calendar;
 
-    Integer i;
-    int j;
+import static java.lang.String.format;
+
+
+class StaticSuper{
+    static {
+        System.out.println("super static block");
+    }
+
+    StaticSuper(){
+        System.out.println("super constructor");
+    }
+}
+
+public class ExercMain extends StaticSuper{
+    static int rand;
+
+    static{
+        rand = (int)(Math.random()*6);
+        System.out.println("static block " + rand);
+    }
+
+    ExercMain(){
+        System.out.println("constructor");
+    }
 
     public static void main(String[] args) {
 
-        ExercMain exerc = new ExercMain();
-        exerc.go();
-    }
-
-    public void go(){
-        j=i;
-        System.out.println(i);
-        System.out.println(j);
+        System.out.println("in main");
+        ExercMain obj = new ExercMain();
     }
 }
